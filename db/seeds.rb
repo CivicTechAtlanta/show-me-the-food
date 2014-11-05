208345a2622b41cbaf77ca9f4d62fb7456020b6e
Shelby Switzer <shelby@voterheads.com>
2014-11-04 21:02:48 -0700

    add source to locations to identify which csv the data is coming from

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@ CSV.parse(open(file), headers: true, :encoding => 'UTF-8') do |row|
                   violations: row["Multiple_Violations"], lot_condition: row["Lot_Condition"],
                   structure_condition: row["Structure_Condition"], digest_year: row["DIGEST"],
                   owner: row["OWNER"], tax_district: row["TAX_DISTR"], objectid_1: row["OBJECTID_1"],
-                  objectid: row["OBJECTID"], val_acres: row["VAL_ACRES"], structure_year: row["STRUCT_YR"]
+                  objectid: row["OBJECTID"], val_acres: row["VAL_ACRES"], structure_year: row["STRUCT_YR"],
+                  source: "Atlanta_Strategic_Community_Investment_2013.csv"
 
 
 end
